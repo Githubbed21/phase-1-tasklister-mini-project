dbaf4721dda4e810a40106674608bd1ac8a9f629
dean <diazdean21@gmail.com>
2021-10-30 14:33:55 -0500

    Completed assignemt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', newToDo)
 });
+
+const newToDo = function(event) {
+  event.preventDefault()
+  let input = event.target['new-task-description'].value
+  let color = event.target.selectPriority.value
+  let date = event.target.dueDate.value
+  console.log(color);
+  appendTasker(input, color, date)
+  event.target['new-task-description'].value = ''
+  event.target.dueDate.value = ''
+}
+
+const appendTasker = function(input, color, date){
+  let ul = document.querySelector('#tasks')
+  let li = document.createElement('li')
+  let deleteBtn = document.createElement('button')
+  
+
+  //assign values
+  li.innerText = `${input} Due: ${date}  `
+  li.style.color = color
+  deleteBtn.innerText = 'X'
+ 
+
+
+  //appending to DOM
+  ul.appendChild(li)
+  li.append(deleteBtn)
+
+
+  // event listeners
+  deleteBtn.addEventListener('click', () => li.remove())
+  
+}
